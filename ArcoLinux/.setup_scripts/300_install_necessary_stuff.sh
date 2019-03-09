0ba150fe36d9eb204edbf3faada8cb34c085954a
Gustavo Diel <gustavodiel@hotmail.com>
2019-03-09 11:51:50 -0300

    Add pacman for main stuff

---
+++ ArcoLinux/.setup_scripts/300_install_necessary_stuff.sh
@@ -0,0 +1,20 @@
+sudo pacman -Syyu --noconfirm
+
+sudo pacman -S --noconfirm --needed \
+vim \
+neovim \
+screenfetch \
+spotify \
+docker \
+docker-compose \
+powerline-fonts \
+libyaml \
+fzy \
+sfml \
+yarn \
+python-urllib2 
+
+
+
+sudo yay -S --noconfirm --needed \
+visual-studio-code-bin \
