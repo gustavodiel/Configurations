8693eaf207cf130e4b39a3152bbd7129f571842d
Gustavo Diel <gustavodiel@hotmail.com>
2019-03-09 11:31:36 -0300

    Create base files

---
+++ ArcoLinux/README.md
@@ -0,0 +1,4 @@
+# This is my setup for ArcoLinux
+
+
+### Inspired by: https://github.com/erikdubois/arcolinux-nemesis
---
+++ ArcoLinux/setup.sh
@@ -0,0 +1,11 @@
+### Execute Necessary Scripts
+
+cd .setup_scripts
+
+sh 000_fix_makepgk_cores.sh
+sh 100_fix_intel_microcode.sh
+sh 200_fix_intel_settings_x11.sh
+sh 300_create_personal_folders.sh
+sh 400_install_necessary_stuff.sh
+sh 500_setup_configs.sh
+sh 600_install_random_stuff.sh
