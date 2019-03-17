9d588096ebb0185161cb635c2a3197dec2fcdbf7
Gustavo Diel <gustavodiel@hotmail.com>
2019-03-17 14:55:07 -0300

    Fixed pacman pacakges

--- ArcoLinux/.setup_scripts/300_install_necessary_stuff.sh
+++ ArcoLinux/.setup_scripts/300_install_necessary_stuff.sh
@@ -7,15 +7,12 @@ set -e
 
 sudo pacman -Syyu --noconfirm
 
-sudo pacman -S --noconfirm --needed base-devel libffi libyaml openssl zlib rbenv postgresql libgit2 vim neovim screenfetch spotify docker docker-compose powerline-fonts libyaml fzy sfml yarn nautilus progress arandr nodejs virtualbox bluez awesome-terminal-fonts
+sudo pacman -S --needed base-devel libffi libyaml openssl zlib postgresql libgit2 vim neovim screenfetch spotify docker docker-compose powerline-fonts libyaml fzy sfml yarn nautilus progress arandr nodejs virtualbox bluez awesome-terminal-fonts
 
-yay -S --noconfirm --needed  ruby-build visual-studio-code-bin google-chrome jetbrains-toolbox sublime-text-dev atom nerd-fonts-source-code-pro xcwd-git
+yay -S --noconfirm --needed rbenv ruby-build visual-studio-code-bin google-chrome jetbrains-toolbox sublime-text-dev atom nerd-fonts-source-code-pro xcwd-git
 
 # Pytohn 3 libs
-pip3 install psutil requests docker pygit2 xkbgroup i3ipc netifaces
-
-# NPM
-npm install -g spaceship-prompt
+sudo pip3 install psutil requests docker pygit2 xkbgroup i3ipc netifaces
 
 echo "################################################################"
 echo "###                     Stuff Installed!                    ####"
